0a5f742ae6d6bd75dd14e885bc483d5d5d98f777
Adeola <ojoadeolagabriel@gmail.com>
2020-12-05 13:05:43 +0000

    first blood

--- delivery/vagrant/scripts/install.sh
+++ delivery/vagrant/scripts/install.sh
@@ -11,4 +11,17 @@ sudo apt-get -y update
 sudo apt install openjdk-11-jdk -y
 
 # install grafana
-docker run -d -p 3000:3000 grafana/grafana
+if ! docker ps --format '{{.Names}}' | grep -w grafana &> /dev/null; then
+    docker run -d -p 3000:3000 --name grafana grafana/grafana
+fi
+
+# install k8s
+sudo snap install microk8s --classic
+alias mk="microk8s.kubectl"
+
+# install kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+alias k=kubectl
