9897790662b282904ab66e86bb7fe28353cdda94
Adeola <ojoadeolagabriel@gmail.com>
2020-12-25 07:35:59 +0000

    first blood 5

--- delivery/ansible/deployment/service/roles/deploy-docker-image/scripts/run.sh
+++ delivery/ansible/deployment/service/roles/deploy-docker-image/scripts/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 IMAGE_HOST_PORT="${1:-8080}"
 PROJECT_ARTIFACT_NAME="${2}"
 VERSION="1.${3:-1.0}"
@@ -8,12 +10,14 @@ IMAGE_NAME="ojoadeolagabriel/${PROJECT_ARTIFACT_NAME}"
 IMAGE_NAME_WITH_VERSION="${IMAGE_NAME}:${VERSION}"
 
 # clear existing image
+# shellcheck disable=SC2039
 if [[ "$(docker images -q "${IMAGE_NAME_WITH_VERSION}" 2> /dev/null)" != "" ]]; then
-  IMAGE_ID=$(docker images --filter=reference=${IMAGE_NAME} --format "{{.ID}}")
-  sudo docker rmi -f ${IMAGE_ID} 2> /dev/null && echo "removed -> ${IMAGE_ID}"
+  IMAGE_ID=$(docker images --filter=reference="${IMAGE_NAME}" --format "{{.ID}}")
+  sudo docker rmi -f "${IMAGE_ID}" 2> /dev/null && echo "removed -> ${IMAGE_ID}"
 else
   echo "not cleaning: ${IMAGE_NAME}:${VERSION}"
 fi
 
-sudo docker rm -f ${PROJECT_ARTIFACT_NAME} 2> /dev/null && echo "removed container"
-sudo docker run -d -p "${IMAGE_HOST_PORT}:${IMAGE_HOST_PORT}" --name ${PROJECT_ARTIFACT_NAME} "${IMAGE_NAME_WITH_VERSION}"
+# deploy container
+sudo docker rm -f "${PROJECT_ARTIFACT_NAME}" 2> /dev/null && echo "removed container"
+sudo docker run -d -p "${IMAGE_HOST_PORT}:${IMAGE_HOST_PORT}" --env-file /etc/"$PROJECT_ARTIFACT_NAME".conf --name "${PROJECT_ARTIFACT_NAME}" "${IMAGE_NAME_WITH_VERSION}"
