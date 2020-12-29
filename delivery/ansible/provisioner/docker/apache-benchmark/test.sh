740892f03487281d3e0c1c5d832c13711709f196
Adeola <ojoadeolagabriel@gmail.com>
2020-12-29 17:11:05 +0000

    temp update to artifactory

--- delivery/ansible/provisioner/docker/apache-benchmark/test.sh
+++ delivery/ansible/provisioner/docker/apache-benchmark/test.sh
@@ -5,4 +5,4 @@ set -ex
 echo "$(pwd) is the current output dir"
 
 docker run --rm -v "$(pwd)"/output:/app/output --name=ab ojoadeolagabriel/ab \
-  "http://host.docker.internal:8095/api/v1/transfer?reference=TRA-543-318-5633-AA/" 20 1
+  "http://host.docker.internal:8095/api/v1/transfer-requests?reference=TRA-543-318-5633-AA/" 20 1
--- delivery/vagrant/scripts/install_docker.sh
+++ delivery/vagrant/scripts/install_docker.sh
@@ -6,4 +6,9 @@ then
     sudo systemctl start docker
     sudo systemctl enable docker
     docker network create local-network &> /dev/null;
+
+    # install compose
+    curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
 fi
