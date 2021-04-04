2681f7240506a638a7bf09e97e31864d784226eb
Adeola <ojoadeolagabriel@gmail.com>
2021-04-04 17:48:45 +0100

    first commit 2

--- delivery_v2/provision/vagrant/scripts/install_redis.sh
+++ delivery_v2/provision/vagrant/scripts/install_redis.sh
@@ -6,4 +6,4 @@ SHARED_NETWORK="${1:-local-network}"
 CONTAINER_NAME=${2:-redis}
 
 docker rm -f "$CONTAINER_NAME" &>/dev/null && echo 'removed redis container'
-docker run --name "$CONTAINER_NAME" --net "$SHARED_NETWORK" -e ALLOW_EMPTY_PASSWORD=yes -d bitnami/redis:latest
+docker run --name "$CONTAINER_NAME" --net "$SHARED_NETWORK" -e ALLOW_EMPTY_PASSWORD=yes -p "6379:6379" -d bitnami/redis:latest
